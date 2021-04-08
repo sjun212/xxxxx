53d48a4286288950bcd6f4554e2c5bc8403b5655
kingle1024 <a951357qW!>
2021-04-08 22:30:02 +0900

    spring boot

---
+++ src/main/java/com/todak/todak/TodakApplication.java
@@ -0,0 +1,13 @@
+package com.todak.todak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodakApplication.class, args);
+	}
+
+}
